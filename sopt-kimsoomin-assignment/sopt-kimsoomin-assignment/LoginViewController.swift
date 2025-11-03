2c2d932a66c419b1f85756195482de7bbb1ca04b
sumchive <haon0319@naver.com>
2025-11-04 00:22:53 +0900

    [refactor]: 코드 주석 추가

--- sopt-kimsoomin-assignment/sopt-kimsoomin-assignment/LoginViewController.swift
+++ sopt-kimsoomin-assignment/sopt-kimsoomin-assignment/LoginViewController.swift
@@ -11,6 +11,13 @@ import SnapKit
 
 final class LoginViewController: UIViewController {
     
+    //이메일 형식 example@aaa.com
+    private let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    //최소 8자, 하나 이상의 문자, 숫자 및 특수 문자
+    private let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$"
+    
+    
     // MARK: - Properties
     
     private let continueWithLabel: UILabel = {
@@ -37,6 +44,8 @@ final class LoginViewController: UIViewController {
         let textField = UITextField()
         return textField.then {
             $0.placeholder = "이메일 아이디"
+            
+            // Placeholder 색상 변경
             $0.attributedPlaceholder = NSAttributedString(string: "이메일 아이디", attributes: [NSAttributedString.Key.foregroundColor : UIColor.baeminGray700])
             $0.borderStyle = .none
             $0.layer.borderColor = UIColor.baeminGray200.cgColor
@@ -45,10 +54,10 @@ final class LoginViewController: UIViewController {
             $0.font = .body_r_14
             $0.textColor = .baeminBlack
             $0.addLeftPadding()
-            
         }
     }()
     
+    // 비밀번호 텍스트필드, x 지우기 버튼, 눈 모양 버튼을 담을 뷰
     private let passwordContainerView: UIView = {
         let view = UIView()
         view.layer.borderColor = UIColor.baeminGray200.cgColor
@@ -124,12 +133,6 @@ final class LoginViewController: UIViewController {
         }
     }()
     
-    //이메일 형식 example@aaa.com
-    private let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    
-    //최소 8자, 하나 이상의 문자, 숫자 및 특수 문자
-    private let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$"
-    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -146,6 +149,7 @@ final class LoginViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // 화면이 나타날 때마다 초기화
         idAndEmailTextField.text = ""
         passwordTextField.text = ""
         checkTextField()
@@ -154,89 +158,6 @@ final class LoginViewController: UIViewController {
         passwordTextField.isSecureTextEntry = true
     }
     
-    private func checkTextFieldIsEmpty() {
-        idAndEmailTextField.addTarget(self, action: #selector(checkTextField), for: .editingChanged)
-        passwordTextField.addTarget(self, action: #selector(checkTextField), for: .editingChanged)
-    }
-    
-    // MARK: - Actions
-    
-    @objc
-    private func checkTextField() {
-        // 텍스트가 비어있지 않은지 확인
-        // 사용자가 앞 뒤에 공백, 줄바꿈 문자를 입력할 수 있기때문에 trimmingCharacters(in: .whitespacesAndNewlines)로 공백 제거
-        
-        let isIdEmailValid = !(idAndEmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
-        let isPasswordValid = !(passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
-        
-        let isButtonEnabled = isIdEmailValid && isPasswordValid
-        
-        loginButton.isEnabled = isButtonEnabled
-        
-        loginButton.backgroundColor = isButtonEnabled ? .baeminMint500 : .baeminGray200
-        
-        clearPasswordButton.isHidden = passwordTextField.text?.isEmpty ?? true
-    }
-    
-    @objc private func textFieldDidChange(_ textField: UITextField) {
-        if textField == passwordTextField {
-            clearPasswordButton.isHidden = textField.text?.isEmpty ?? true
-        }
-    }
-    
-    // 이메일, 비밀번호 유효성 검사
-    private func isValid(text: String, regex: String) -> Bool {
-        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
-        return predicate.evaluate(with: text)
-    }
-    
-    // Alert 표시
-    private func presentAlert(message: String) {
-        let alert = UIAlertController(title: "입력 오류", message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "확인", style: .default)
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
-    
-    @objc private func clearPasswordButtonDidTap() {
-        passwordTextField.text = ""
-        checkTextField()
-    }
-    
-    @objc private func securityToggleButtonDidTap() {
-        passwordTextField.isSecureTextEntry.toggle()
-        let imageName = passwordTextField.isSecureTextEntry ? "eye-slash" : "eye"
-        securityToggleButton.setImage(UIImage(named: imageName), for: .normal)
-    }
-    
-    
-    private func pushToWelcomeVC(){
-        let vc = GreetingViewController()
-        vc.id = idAndEmailTextField.text
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @objc
-    private func loginButtonDidTap() {
-        guard let idText = idAndEmailTextField.text,
-              let passwordText = passwordTextField.text else { return }
-        
-        let trimmedId = idText.trimmingCharacters(in: .whitespacesAndNewlines)
-        let trimmedPassword = passwordText.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        // 이메일 검사
-        if !isValid(text: trimmedId, regex: emailRegex) {
-            presentAlert(message: "이메일 형식이 달라요")
-            return
-        }
-        
-        // 비밀번호 유효성 검사
-        if !isValid(text: trimmedPassword, regex: passwordRegex) {
-            presentAlert(message: "비밀번호는 최소 8자 이상, 영문, 숫자, 특수문자를 포함해야 해요.")
-            return
-        }
-        pushToWelcomeVC()
-    }
     
     // MARK: - Layout
     
@@ -316,5 +237,100 @@ final class LoginViewController: UIViewController {
             $0.top.equalTo(loginButton.snp.bottom).offset(28)
             $0.leading.equalTo(findAccountLabel.snp.trailing).offset(1)
         }
+    }
+    
+    
+    // MARK: - Actions
+    
+    // 텍스트가 비어있지 않은지 확인
+    @objc
+    private func checkTextField() {
+        // 사용자가 앞 뒤에 공백, 줄바꿈 문자를 입력할 수 있기때문에 trimmingCharacters(in: .whitespacesAndNewlines)로 공백 제거
+        
+        let isIdEmailValid = !(idAndEmailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
+        let isPasswordValid = !(passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true)
+        
+        let isButtonEnabled = isIdEmailValid && isPasswordValid
+        
+        loginButton.isEnabled = isButtonEnabled
+        
+        loginButton.backgroundColor = isButtonEnabled ? .baeminMint500 : .baeminGray200
+        
+        clearPasswordButton.isHidden = passwordTextField.text?.isEmpty ?? true
+    }
+    
+    // passwordField에 값이 입력되었을 때
+    @objc
+    private func textFieldDidChange(_ textField: UITextField) {
+        if textField == passwordTextField {
+            clearPasswordButton.isHidden = textField.text?.isEmpty ?? true
+        }
+    }
+    
+    //x 버튼 액션
+    @objc
+    private func clearPasswordButtonDidTap() {
+        passwordTextField.text = ""
+        checkTextField()
+    }
+    
+    // 눈 버튼 액션
+    @objc private func securityToggleButtonDidTap() {
+        passwordTextField.isSecureTextEntry.toggle()
+        let imageName = passwordTextField.isSecureTextEntry ? "eye-slash" : "eye"
+        securityToggleButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    
+    // 로그인버튼 액션
+    @objc
+    private func loginButtonDidTap() {
+        guard let idText = idAndEmailTextField.text,
+              let passwordText = passwordTextField.text else { return }
+        
+        let trimmedId = idText.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmedPassword = passwordText.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // 이메일 검사
+        if !isValid(text: trimmedId, regex: emailRegex) {
+            presentAlert(message: "이메일 형식이 달라요")
+            return
+        }
+        
+        // 비밀번호 유효성 검사
+        if !isValid(text: trimmedPassword, regex: passwordRegex) {
+            presentAlert(message: "비밀번호는 최소 8자 이상, 영문, 숫자, 특수문자를 포함해야 해요.")
+            return
+        }
+        pushToWelcomeVC()
+    }
+    
+    
+    // MARK: - Functions
+    
+    // 아이디, 비번 필드 확인
+    private func checkTextFieldIsEmpty() {
+        idAndEmailTextField.addTarget(self, action: #selector(checkTextField), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(checkTextField), for: .editingChanged)
+    }
+    
+    // 이메일, 비밀번호 유효성 검사
+    private func isValid(text: String, regex: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: text)
+    }
+    
+    // Alert 표시
+    private func presentAlert(message: String) {
+        let alert = UIAlertController(title: "입력 오류", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    // 환영합니다 뷰로 이동
+    private func pushToWelcomeVC(){
+        let vc = GreetingViewController()
+        vc.id = idAndEmailTextField.text
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
