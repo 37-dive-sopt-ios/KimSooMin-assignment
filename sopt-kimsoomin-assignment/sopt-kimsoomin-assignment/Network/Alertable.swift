119f41b04bd5778d62ae765ae9ddc445dc223c50
sumchive <haon0319@naver.com>
2025-11-21 02:30:24 +0900

    [add] 네트워크 기본 파일 추가

---
+++ sopt-kimsoomin-assignment/sopt-kimsoomin-assignment/Network/Alertable.swift
@@ -0,0 +1,24 @@
+//
+//  Alertable.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 11/8/25.
+//
+
+import UIKit
+
+public protocol Alertable: AnyObject {
+    func showAlert(title: String, message: String, completion: (() -> Void)?)
+}
+
+// MARK: - 기본 구현
+
+public extension Alertable where Self: UIViewController {
+    func showAlert(title: String, message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { _ in
+            completion?()
+        })
+        self.present(alert, animated: true)
+    }
+}
---
+++ sopt-kimsoomin-assignment/sopt-kimsoomin-assignment/Network/RegisterRequest.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterRequest.swift
+//  sopt-ios-seminar01
+//
+//  Created by mandoo on 11/8/25.
+//
+
+import Foundation
+
+struct RegisterRequest: Encodable {
+    let username: String
+        let password: String
+        let name: String
+        let email: String
+        let age: Int
+}
